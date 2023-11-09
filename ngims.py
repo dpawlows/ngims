04e2ab0545fee782015d3666aa53991edfd08d23
Dave Pawlowski <dpawlows@emich.edu>
2023-11-09 14:03:56 -0500

    updates

--- ngims.py
+++ ngims.py
@@ -1,14 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from glob import glob
-from matplotlib import pyplot as pp
 import datetime
+from numpy import mean
+import pandas as pd 
+from matplotlib import pyplot as pp
 
 """
-Classes and functions for reading and working with MAVEN NGIMS data
+Classes and functions for reading and working with MAVEN NGIMS data.
+Added a quicker method to simply load a pandas data frame.
 """
 
 class DataLoader():
+
     """Class for handling filtering NGIMS data"""
 
     def get(self,data,**kwargs):
@@ -17,18 +21,108 @@ class DataLoader():
 
         return data
 
+    def orbits(self,data,output=False):
+        sorbit = data[0]['orbit']
+        iorbits = [0]
+        i = 0
+        for d in data:
+            if d['orbit'] != sorbit:
+                iorbits.append(i)
+                sorbit = d['orbit']
+            i += 1
 
-def readCSN(file):
+        orbits = []
+        time = []
+        for i in range(len(iorbits[0:-1])):
+            orbits.append(data[iorbits[i]]['orbit'])
+            iavg = int((iorbits[i]+iorbits[i+1])/2)
+            time.append(data[iavg]['time'])
+
+        if output:
+            f = open('orbits.dat','w')
+            for i in range(len(orbits)):
+                f.write('{:02d} {:02d} {:02d} {:02d} {:02d} {:02d} {:04d}\n'.format(
+                time[i].year,time[i].month,time[i].day,time[i].hour,
+                time[i].minute,time[i].second,orbits[i]
+                ))
+        return {'time':time,"orbits":orbits}
+
+
+
+def getCSN(files,outbound=True):
+    if len(files) > 0:
+        data = []
+        for f in files:
+            d = readCSN(f,outbound)
+            data += d
+
+        return data
+    else:
+        print("No files found")
+        print("In function ngims.getCSN")
+        print("Are the files L2? Is the version correct?")
+        exit(1)
+
+def readNGIMS(file): 
+    '''Given a file, load and return a pandas data frame'''
+    data = pd.read_csv(file)
+    return(data)
+
+def getorbit(file):
+    f = open(file,'r')
+    temp = f.readline().split(',')
+    orbitindex = temp.index('orbit')
+    timeindex = temp.index('t_utc')
+    tidindex = temp.index('tid')
+
+    temp = f.readline().split(',')
+    tid = int(temp[tidindex])
+    orbit = int(temp[orbitindex])
+    time = datetime.datetime(int(temp[timeindex][0:4]),\
+            int(temp[timeindex][5:7]),\
+            int(temp[timeindex][8:10]),int(temp[timeindex][11:13]),\
+            int(temp[timeindex][14:16]),int(temp[timeindex][17:19]))
+    f.close()
+    return {'orbit':orbit,'time':time,'tid':tid}
+
+def writeCSVOrbits(filelist):
+    '''Quickly write the orbit numbers for a list of files to orbits.txt
+    Usage: writeCSVOrbits(filelist)
+        Required: filelist : a list of csv filenames to be read
+
+    '''
+    pos = filelist[0].rfind('csv')
+    filelist.sort(key = lambda x: x[pos-30:pos-25])
+    g = open('orbits.txt','w')
+    g.write('Year Mon Day Hr Min Sec OrbitNum tid\n')
+    for file in filelist:
+        
+        data = getorbit(file)
+        orbit = data['orbit']
+        time = data['time']
+        g.write('{:02d} {:02d} {:02d} {:02d} {:02d} {:02d} {:04d} {:05d}\n'.format(
+                time.year,time.month,time.day,time.hour,
+                time.minute,time.second,orbit,data['tid']
+                ))
+    g.close()
+
+def readCSN(file,outbound):
     '''Function to read NGIMS data
     Usage: data = readCSN(file)
     file: required input, name of file to be read
     data is a list of dicts; each dict contains a single measurement
     and relevant metadata.
+    outbound: true to include outbound data
     '''
 
     data = []
     f=open(file,'r')
-    print file
+    if file.find('ion') > -1:
+        specieskey = 'ion_mass'
+    else:
+        specieskey = "species"
+
+    print(file)
     notStarted = True
     while notStarted:
         line = f.readline()
@@ -39,11 +133,11 @@ def readCSN(file):
     varMap = {}
     for value in range(len(t)):
         varMap[t[value]] = value
-
+    oldalt = 1.0e7
+    oldspecies = ''
     for line in f:
 
         t = line.split(',')
-
         try:
             int(t[0][0])
             time = datetime.datetime(int(t[varMap['t_utc']][0:4]),\
@@ -51,30 +145,39 @@ def readCSN(file):
                 int(t[varMap['t_utc']][8:10]),int(t[varMap['t_utc']][11:13]),\
                 int(t[varMap['t_utc']][14:16]),int(t[varMap['t_utc']][17:19]))
 
-            data.append({'species':t[varMap['species']],
-                'alt':float(t[varMap['alt']]),
-                'lon':float(t[varMap['long']]),
-                'lat':float(t[varMap['lat']]),
-                'precision':float(t[varMap['precision']]),
-                'sza':float(t[varMap['sza']]),
-                'density':float(t[varMap['abundance']]),
-                'time':time,
-                'orbit':int(t[varMap['orbit']]),
-                })
+            alt = float(t[varMap['alt']])
+            species = t[varMap[specieskey]]
+            if (outbound and alt < 300) or (alt < oldalt or (species != oldspecies
+            and alt < 300)):
+                data.append({'species':species,
+                    'alt':alt,
+                    'lon':float(t[varMap['long']]),
+                    'lat':float(t[varMap['lat']]),
+                    'precision':float(t[varMap['precision']]),
+                    'sza':float(t[varMap['sza']]),
+                    'density':float(t[varMap['abundance']]),
+                    'time':time,
+                    'orbit':int(t[varMap['orbit']]),
+                    })
+                oldalt = alt
+                oldspecies = species
         except:
             pass
 
     f.close()
     return data
 
-def getfiles(start,end,version=None):
-    filelist = glob('*csn*.csv')
+def getfiles(start,end,type='csn',version=None,dir=''):
+    if type != 'csn' and type != 'ion':
+        print('type: {} is not recognized'.format(type))
+        exit(1)
+
+    filelist = glob(dir+'mvn_ngi*'+type+'*.csv')
 
     sdate = datetime.date(int(start[0:4]),int(start[4:6]),int(start[6:8]))
     edate = datetime.date(int(end[0:4]),int(end[4:6]),int(end[6:8]))
-
     if version == None:
-        [f for f in filelist if sdate <= \
+        results = [f for f in filelist if sdate <= \
         datetime.date(int(f[-27:-23]),int(f[-23:-21]),int(f[-21:-19]))\
         and edate >=  datetime.date(int(f[-27:-23]),\
             int(f[-23:-21]),int(f[-21:-19]))]
@@ -84,5 +187,8 @@ def getfiles(start,end,version=None):
         datetime.date(int(f[-27:-23]),int(f[-23:-21]),int(f[-21:-19]))\
         and edate >=  datetime.date(int(f[-27:-23]),\
             int(f[-23:-21]),int(f[-21:-19])) and (version in f or version == None) ]
+
+    pos = filelist[0].rfind('csv')
+    results.sort(key = lambda x: x[pos-30:pos-25]) #sort by tid
 
     return results
--- ngimsGITM.py
+++ ngimsGITM.py
@@ -1,26 +1,268 @@
 """
-Functions and variables useful for working with NGIMS data and the M-GITM 
+Functions and variables useful for working with NGIMS data and the M-GITM
 model simultaneously...
 """
 
 import ngims
+import pdb
+from datetime import datetime, timedelta
+from numpy import where, array
+import traceback
 
-def makeSatelliteFile(data,satfile = 'satellite.dat'):
-	"""Creates a satellite file for use with M-GITM.
-	makeSatelliteFile(data,satfile = 'satellite.dat')
-	data is ngims data list."""
+def outputNGIMS(data,output='ngims.dat',*args,**kwargs):
+	'''Creates a data file containing ngims data.
+	outputOneOrbit(orbit,output='ngims.dat')
+	data: ngims data list
+	output='ngims.dat': name of output file
+	'''
 
-        print "Making satellite file...."
-        dataloader = ngims.DataLoader()
-        satdata = dataloader.get(data,species='Ar')
+	altres = 5 #km
+	f = open(output,'w')
+	altold = 1.0e6
+	orbitold = 0
+	for d in data:
 
-        f = open(satfile,'w')
-        f.write('#START\n')
+		alt = d['alt']
+		time = d['time']
+		orbit = d['orbit']
+		d['lon'] = d['lon']+360 if d['lon'] < 0 else d['lon']
 
-        for d in satdata:
-            time = d['time']
-            f.write('{:4d}\t{:02d}\t{:02d}\t{:02d}\t{:02d}\t{:02d}\t{:4.1f}\t{:4.1f}\t{:4.1f}\n'.format(time.year,time.month,time.day,\
-                time.hour,time.minute,time.second,d['lon'],d['lat'],d['alt']))
+		if not 'precision' in kwargs or d['precision'] < kwargs['precision']:
+			if (altold-alt) > altres or orbitold != orbit:
+
+				f.write('{:4d}  {:02d}  {:02d}  {:02d}  {:02d}  {:02d}  {:02d}\
+				{:8.1f}{:8.1f}{:8.1f}   {:4d}    {:8.3g}\n'.format(time.year,time.month,time.day,\
+					time.hour,time.minute,time.second,0,d['lon'],d['lat'],alt,\
+					orbit,d['density']))
+
+				altold = alt
+				orbitold = orbit
+
+	return 1
+	f.close()
+
+def makeSatelliteFile(data,species,satfile = 'sat.dat',*args,**kwargs):
+	"""Creates a satellite file for use with M-GITM. Can average the data since NGIMS observations are every 
+		few seconds.
+	makeSatelliteFile(data,satfile = 'satellite.dat,*args,**kwargs) 
+	data: ngims data list
+	satfile = 'satfile.dat': name of satellite file
+	args:
+	kwargs:
+		timeAveraging=averagingTime: will average data over averagingTime seconds
+		locAveraging=averagingLoc: will average data over averagingLoc km
+		location=locationlist; output will be 1 satfile per location. Each location 
+			will have one lat, lon, alt per orbit
+		locationtype=locationtype (alt lon or lat); required if location kw exists
 
 
-        return satdata
+	"""
+
+	print("Making satellite file.... ")
+
+	isAveraging = False
+	isTimeAveraging = False
+	isLocAveraging = False
+
+	if 'timeAveraging' in kwargs:
+		isAveraging = True
+		isTimeAveraging = True
+		averagingTime = int(kwargs['timeAveraging'])
+		print("Time Averaging... ",averagingTime)
+
+	if 'locAveraging' in kwargs:
+		isAveraging = True
+		isLocAveraging = True
+		averagingLoc = int(kwargs['locAveraging'])
+		print("Loc Averaging...",averagingLoc)
+
+
+	if 'location' in kwargs:
+		try:
+			locationType = kwargs['locationType'].lower()
+			if locationType == 'lat' or locationType == 'latitude':
+				locationType = 'lat'
+			elif locationType == 'lon' or locationType == 'longitude':
+				locationType = 'lon'
+			elif locationType == 'alt' or locationType == 'altitude':
+				locationType = 'alt'
+			else:
+				print("invalid locationType in makeSatelliteFile")
+				print("stopping in ngimsGITM.py")
+				exit(1)
+
+		except:
+			print("must specify locationtype in makeSatelliteFile")
+			print("stopping in ngimsGITM.py")
+			exit(1)
+
+		isLocs = True
+		locations = kwargs['location']
+		nlocations = len(locations)
+		print("getting locations: ",locations)
+	else:
+		isLocs = False
+		nlocations = 1
+
+
+	dataloader = ngims.DataLoader()
+
+	# satdata = dataloader.get(data,species=species)
+	satdata = data
+
+	if isAveraging:
+		newdata = []
+		if isTimeAveraging:
+			oldtime = datetime(1900,1,1,0,0,0)
+			tempdata = []
+
+			for d in satdata:
+				time = d['time']
+				lat = d['lat']
+				lon = d['lon']
+				alt = d['alt']
+				dt = time-oldtime
+				if dt.total_seconds() > averagingTime*60.0:
+					if len(tempdata) > 1:
+						lons = array([temp['lon'] for temp in tempdata])
+
+						if min(lons) < -170 and max(lons) > 170:
+							#can't average like normal because we are
+							#jumping from +180 to -180!
+							nlocs = where(lons < 0)
+							lons[nlocs] = lons[nlocs]+360.
+							alon = sum(lons)/len(lons)
+							alon = alon - 360 if alon > 180 else alon
+						else:
+							alon = (sum(temp['lon'] for temp in tempdata)/len(tempdata))
+
+
+						newdata.append({
+						'time':tempdata[len(tempdata)/2]['time'],
+						'lat':(sum(temp['lat'] for temp in tempdata)/len(tempdata)),
+						'lon':alon,
+						'alt':(sum(temp['alt'] for temp in tempdata)/len(tempdata)),
+						})
+
+					tempdata = []
+					oldtime = time
+
+				tempdata.append({
+				'time':time,
+				'alt':alt,
+				'lat':lat,
+				'lon':lon
+				})
+
+			satdata = newdata
+
+
+		if isLocAveraging:
+			#We are averaging over altitude! 
+			newdata = []
+			alts = []
+			oldd = satdata[0]
+			tempdata=[]
+			for d in satdata:
+				time = d['time']
+				lat = d['lat']
+				lon = d['lon']
+				alt = d['alt']
+				alts.append(alt)
+				# pdb.set_trace()
+				if abs(alts[-1]-alts[0]) > averagingLoc or d['orbit'] != d['orbit']:
+					if len(tempdata) > 1:
+						lons = array([temp['lon'] for temp in tempdata])
+						if min(lons) < -170 and max(lons) > 170:
+							#can't average like normal because we are
+							#jumping from +180 to -180!
+							nlocs = where(lons < 0)
+							lons[nlocs] = lons[nlocs]+360.
+							alon = sum(lons)/len(lons)
+							alon = alon - 360 if alon > 180 else alon
+						else:
+							alon = (sum(temp['lon'] for temp in tempdata)/len(tempdata))
+
+						# pdb.set_trace()
+						newdata.append({
+						'time':tempdata[int(len(tempdata)/2)]['time'],
+						'lat':(sum(temp['lat'] for temp in tempdata)/len(tempdata)),
+						'lon':alon,
+						'alt':(sum(temp['alt'] for temp in tempdata)/len(tempdata)),
+						})
+
+					tempdata = []
+					alts = []
+					oldd = d
+
+				tempdata.append({
+				'time':time,
+				'alt':alt,
+				'lat':lat,
+				'lon':lon
+				})
+
+			satdata = newdata
+
+
+	if isLocs:
+		#Getting data at specific locations
+
+		oldd = satdata[0]
+		tempdata = [[] for _ in range(nlocations)]
+		for d in satdata:
+			for iloc in range(nlocations):
+				if (d[locationType]-locations[iloc])*(oldd[locationType]-locations[iloc]) < 0 \
+				and oldd['orbit'] == d['orbit']:
+
+					if abs(d[locationType] - oldd[locationType]) > 2.0:
+						print("Warning!!! Locations are quite different.  This is unexpected\
+						in ngitmsGITM.py.  I assume you know what you are doing.  Proceeding optimistically.")
+						breakpoint()
+
+					if abs(d[locationType]-locations[iloc]) < abs(oldd[locationType]-locations[iloc]):
+						time = d['time']
+						lat = d['lat']
+						lon = d['lon']
+						alt = d['alt']
+
+					else:
+						time = oldd['time']
+						lat = oldd['lat']
+						lon = oldd['lon']
+						alt = oldd['alt']
+
+					tempdata[iloc].append({
+					'time':time,
+					'alt':alt,
+					'lat':lat,
+					'lon':lon
+					})
+
+			oldd = d
+
+		satdata = tempdata
+
+
+	suffixloc = satfile.find('.dat')
+	for iloc in range(nlocations):
+		if suffixloc > -1 and nlocations > 1:
+			thissatfile = satfile[0:suffixloc]+'_0'+str(iloc)+satfile[suffixloc:]
+		else:
+			thissatfile = satfile
+		f = open(thissatfile,'w')
+		f.write('#START\n')
+		if nlocations > 1:
+			tempdata = satdata[iloc]
+		else:
+			tempdata = satdata
+
+		for d in tempdata:
+			time = d['time']
+			d['lon'] = d['lon']+360 if d['lon'] < 0 else d['lon']
+			f.write('{:4d}  {:02d}  {:02d}  {:02d}  {:02d}  {:02d}  {:02d}\
+			{:8.1f}{:8.1f}{:8.1f}\n'.format(time.year,time.month,time.day,\
+				time.hour,time.minute,time.second,0,d['lon'],d['lat'],d['alt']))
+
+
+	return satdata
